b436efb8cdae7ece376301588408bbf64edfd1f5
kafkaphoenix <jaguilerapuerta@gmail.com>
2024-07-04 01:25:01 +0200

    feat: 1.22 wildcard with templ

--- components_templ.go
+++ components_templ.go
@@ -74,3 +74,57 @@ func notFoundComponent() templ.Component {
 		return templ_7745c5c3_Err
 	})
 }
+
+var greeting = "Welcome!"
+
+func helloComponent(name string) templ.Component {
+	return templruntime.GeneratedTemplate(func(templ_7745c5c3_Input templruntime.GeneratedComponentInput) (templ_7745c5c3_Err error) {
+		templ_7745c5c3_W, ctx := templ_7745c5c3_Input.Writer, templ_7745c5c3_Input.Context
+		templ_7745c5c3_Buffer, templ_7745c5c3_IsBuffer := templruntime.GetBuffer(templ_7745c5c3_W)
+		if !templ_7745c5c3_IsBuffer {
+			defer func() {
+				templ_7745c5c3_BufErr := templruntime.ReleaseBuffer(templ_7745c5c3_Buffer)
+				if templ_7745c5c3_Err == nil {
+					templ_7745c5c3_Err = templ_7745c5c3_BufErr
+				}
+			}()
+		}
+		ctx = templ.InitializeContext(ctx)
+		templ_7745c5c3_Var4 := templ.GetChildren(ctx)
+		if templ_7745c5c3_Var4 == nil {
+			templ_7745c5c3_Var4 = templ.NopComponent
+		}
+		ctx = templ.ClearChildren(ctx)
+		_, templ_7745c5c3_Err = templ_7745c5c3_Buffer.WriteString("<div><h1>")
+		if templ_7745c5c3_Err != nil {
+			return templ_7745c5c3_Err
+		}
+		var templ_7745c5c3_Var5 string
+		templ_7745c5c3_Var5, templ_7745c5c3_Err = templ.JoinStringErrs(name)
+		if templ_7745c5c3_Err != nil {
+			return templ.Error{Err: templ_7745c5c3_Err, FileName: `components.templ`, Line: 17, Col: 14}
+		}
+		_, templ_7745c5c3_Err = templ_7745c5c3_Buffer.WriteString(templ.EscapeString(templ_7745c5c3_Var5))
+		if templ_7745c5c3_Err != nil {
+			return templ_7745c5c3_Err
+		}
+		_, templ_7745c5c3_Err = templ_7745c5c3_Buffer.WriteString("</h1><h2>\"")
+		if templ_7745c5c3_Err != nil {
+			return templ_7745c5c3_Err
+		}
+		var templ_7745c5c3_Var6 string
+		templ_7745c5c3_Var6, templ_7745c5c3_Err = templ.JoinStringErrs(greeting)
+		if templ_7745c5c3_Err != nil {
+			return templ.Error{Err: templ_7745c5c3_Err, FileName: `components.templ`, Line: 18, Col: 19}
+		}
+		_, templ_7745c5c3_Err = templ_7745c5c3_Buffer.WriteString(templ.EscapeString(templ_7745c5c3_Var6))
+		if templ_7745c5c3_Err != nil {
+			return templ_7745c5c3_Err
+		}
+		_, templ_7745c5c3_Err = templ_7745c5c3_Buffer.WriteString("\"</h2></div>")
+		if templ_7745c5c3_Err != nil {
+			return templ_7745c5c3_Err
+		}
+		return templ_7745c5c3_Err
+	})
+}
--- main.go
+++ main.go
@@ -7,9 +7,34 @@ import (
 	"github.com/a-h/templ"
 )
 
+func FuncTimeHandler(now func() time.Time) TimeHandler {
+	return TimeHandler{Now: now}
+}
+
+type TimeHandler struct {
+	Now func() time.Time
+}
+
+func (nh TimeHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	timeComponent(nh.Now()).Render(r.Context(), w)
+}
+
+func FuncHelloHandler() HelloHandler {
+	return HelloHandler{}
+}
+
+type HelloHandler struct {
+}
+
+func (hh HelloHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	name := r.PathValue("name")
+	helloComponent(name).Render(r.Context(), w)
+}
+
 func main() {
-	http.Handle("/", templ.Handler(timeComponent(time.Now())))
+	http.Handle("/", FuncTimeHandler(time.Now))
 	http.Handle("/404", templ.Handler(notFoundComponent(), templ.WithStatus(http.StatusNotFound)))
+	http.Handle("/hello/{name}", FuncHelloHandler())
 
 	http.ListenAndServe(":8080", nil)
 }
